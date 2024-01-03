82547e563aeed32f7ad82b11b157d5cfc41d6504
parth <parthshinde@gmail.com>
2024-01-03 20:07:14 -0300

    phone info update

--- oops.cpp
+++ oops.cpp
@@ -20,6 +20,24 @@ public:
         cout << type;
         cout << name;
         cout << color;
+        cout<<endl;
+    }
+};
+class Mobile
+{
+public:
+    string type;
+    string color;
+    int ram;
+    string name;
+    int price;
+    void printMobile()
+    {
+        cout<<type<<endl;
+        cout<<color<<endl;
+        cout<<ram<<"gb"<<endl;
+        cout<<name<<endl;
+        cout<<price<<endl;
     }
 };
 int main()
@@ -29,5 +47,18 @@ int main()
     prajakta.type = "Ball Pen";
     prajakta.color = "Blue";
     prajakta.printPen();
+    Pen p;
+    p.name = "unomax";
+    p.type = "Ball Pen";
+    p.color = "blue";
+    p.printPen();
+    Mobile parth;
+    parth.name = "asus";
+    parth.type = "Android";
+    parth.color = "silver";
+    parth.ram = 4;
+    parth.price = 13000;
+    parth.printMobile();
+
     return 0;
 }
