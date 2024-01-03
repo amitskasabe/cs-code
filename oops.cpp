499cfc1aa67ea1762c821b84f3a16a23f9b2e1f1
prajakta <prajakta_mhetre@outlook.com>
2024-01-03 20:05:53 +0530

    classandobjects

--- oops.cpp
+++ oops.cpp
@@ -17,11 +17,26 @@ public:
     string color;
     void printPen()
     {
-        cout << type;
-        cout << name;
-        cout << color;
+        cout << type << endl;
+        cout << name << endl;
+        cout << color << endl;
     }
 };
+class Mobile
+{
+public:
+    string name;
+    string color;
+    string type;
+    int price;
+    void printMobile()
+    {
+        cout << name << endl;
+        cout << color << endl;
+        cout << type << endl;
+        cout << price << endl;
+    }
+} ;
 int main()
 {
     Pen prajakta;
@@ -29,5 +44,16 @@ int main()
     prajakta.type = "Ball Pen";
     prajakta.color = "Blue";
     prajakta.printPen();
+    Pen siyana;
+    siyana.name = "airmail";
+    siyana.type = "ink pen";
+    siyana.color = "blue";
+    siyana.printPen();
+    Mobile prajakta1;
+    prajakta1.name = "samsung";
+    prajakta1.color = "blue";
+    prajakta1.type = "android";
+    prajakta1.price = 20000;
+    prajakta1.printMobile();
     return 0;
 }
