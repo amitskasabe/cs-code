9e54ba5d2251e4a74990fd37dc870e6cbcb17ac0
amitkasabe <amit.kasabe25@gmail.com>
2024-01-10 18:38:52 +0530

    vector code is been pushed

--- vector/example-2.cpp
+++ vector/example-2.cpp
@@ -5,7 +5,7 @@ vector<int> bubbleSort(vector<int> &arr)
 {
     for (int i = 0; i < arr.size(); i++)
     {
-        for (int j = i + 1; j <arr.size(); j++)
+        for (int j = i + 1; j < arr.size(); j++)
         {
             if (arr[i] > arr[j])
             {
@@ -14,13 +14,14 @@ vector<int> bubbleSort(vector<int> &arr)
         }
     }
     return arr;
-    
 }
 
 int main()
 {
     int arr[] = {1, 10, 23, 18, 20, 50};
-    vector<int> sortedArray = bubbleSort(arr);
+    vector<int> vec(arr, arr + sizeof(arr) / sizeof(arr[0]));
+
+    vector<int> sortedArray = bubbleSort(vec);
     for (auto itr : sortedArray)
     {
         cout << itr << " ";
